543f37905d1a85397edc7035dfe822677f19ce72
Fardin15 <mohammedshowrav15@gmail.com>
2024-06-10 16:53:02 +0600

    delete server complete for adoption request

--- index.js
+++ index.js
@@ -198,6 +198,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete request
+    app.delete("/adoption/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await adoptionCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
